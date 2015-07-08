c9ae4e1dc2ae17cd21883e90343ab9b52f933c54
Guannan Xie <guannanxie@lbl.gov>
2015-07-08 16:26:50 -0700

    add EventPlane staff, also astyle

--- StPicoMixedEventMaker/StMixerEvent.cxx
+++ StPicoMixedEventMaker/StMixerEvent.cxx
@@ -2,33 +2,33 @@
 
 #include <limits>
 StMixerEvent::StMixerEvent() :  mVtx(StThreeVectorF()),
-    mBField(std::numeric_limits<float>::quiet_NaN())
+   mBField(std::numeric_limits<float>::quiet_NaN())
 {
 }
 StMixerEvent::StMixerEvent(StMixerEvent *t) : mVtx(t->mVtx), mBField(t->mBField),
-					      mTracks(t->mTracks),
-					      mEventKaons(t->mEventKaons), mEventPions(t->mEventPions)
+   mTracks(t->mTracks),
+   mEventKaons(t->mEventKaons), mEventPions(t->mEventPions)
 {
 }
 StMixerEvent::StMixerEvent(StThreeVectorF vtx, float b) :  mVtx(StThreeVectorF()),
-    mBField(std::numeric_limits<float>::quiet_NaN())
+   mBField(std::numeric_limits<float>::quiet_NaN())
 {
-    mVtx = vtx;
-    mBField = b;
+   mVtx = vtx;
+   mBField = b;
 
 }
 void StMixerEvent::addTrack(StMixerTrack t)
 {
-  mTracks.push_back(t);
-  return;
+   mTracks.push_back(t);
+   return;
 }
 void StMixerEvent::addPion(int arrayId)
 {
-  mEventPions.push_back(arrayId);
-  return;
+   mEventPions.push_back(arrayId);
+   return;
 }
 void StMixerEvent::addKaon(int arrayId)
 {
-  mEventKaons.push_back(arrayId);
-  return;
+   mEventKaons.push_back(arrayId);
+   return;
 }
--- StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
+++ StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
@@ -14,144 +14,192 @@
 #include "StPicoMixedEventMaker.h"
 #include "StPicoEventMixer.h"
 #include "StRoot/StRefMultCorr/StRefMultCorr.h"
+#include "StRoot/StEventPlane/StEventPlane.h"
 
 #include <vector>
 
 ClassImp(StPicoMixedEventMaker)
 
 // _________________________________________________________
-StPicoMixedEventMaker::StPicoMixedEventMaker(char const* name, StPicoDstMaker* picoMaker, StRefMultCorr* grefmultCorrUtil,
-        char const* outputBaseFileName,  char const* inputHFListHFtree = "") :
-    StMaker(name), mPicoDst(NULL), mPicoDstMaker(picoMaker),  mPicoEvent(NULL),
-    mGRefMultCorrUtil(grefmultCorrUtil),
-    mOuputFileBaseName(outputBaseFileName), mInputFileName(inputHFListHFtree),
-    mEventCounter(0), mTree(NULL), mOutputFileTree(NULL) {
+StPicoMixedEventMaker::StPicoMixedEventMaker(char const* name, StPicoDstMaker* picoMaker, StRefMultCorr* grefmultCorrUtil, StEventPlane* eventPlaneMaker,
+      char const* outputBaseFileName,  char const* inputHFListHFtree = "") :
+   StMaker(name), mPicoDst(NULL), mPicoDstMaker(picoMaker),  mPicoEvent(NULL),
+   mGRefMultCorrUtil(grefmultCorrUtil), mEventPlane(eventPlaneMaker),
+   mOuputFileBaseName(outputBaseFileName), mInputFileName(inputHFListHFtree),
+   mEventCounter(0), mTree(NULL), mOutputFileTree(NULL)
+{
+   for (int iVz = 0 ; iVz < 10 ; ++iVz)
+   {
+      for (int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality)
+      {
+         mPicoEventMixer[iVz][iCentrality] = NULL;
+      }
+   }
 
-  TH1::AddDirectory(false);
+   TH1::AddDirectory(false);
 
-    mOutputFileTree = new TFile(Form("%s.picoMEtree.root", mOuputFileBaseName.Data()), "RECREATE");
-    mOutputFileTree->SetCompressionLevel(1);
-    mOutputFileTree->cd();
-    // -- create OutputTree
-    int BufSize = (int)pow(2., 16.);
-    int Split = 1;
-    //if (!mTree)
-      //    mTree = new TTree("T", "T", BufSize);
-    //mTree->SetAutoSave(1000000); // autosave every 1 Mbytes
-    //  mTree->Branch("mixedEvent", "StPicoMixedEvent", &mPicoEventMixer, BufSize, Split);
+   mOutputFileTree = new TFile(Form("%s.picoMEtree.root", mOuputFileBaseName.Data()), "RECREATE");
+   mOutputFileTree->SetCompressionLevel(1);
+   mOutputFileTree->cd();
+   // -- create OutputTree
+   int BufSize = (int)pow(2., 16.);
+   int Split = 1;
+   //if (!mTree)
+   //    mTree = new TTree("T", "T", BufSize);
+   //mTree->SetAutoSave(1000000); // autosave every 1 Mbytes
+   //  mTree->Branch("mixedEvent", "StPicoMixedEvent", &mPicoEventMixer, BufSize, Split);
 
-    // -- constructor
+   // -- constructor
 }
 
 // _________________________________________________________
-StPicoMixedEventMaker::~StPicoMixedEventMaker() {
-  cout <<"Lomnitz:: Destroying"<<endl;
-  delete mGRefMultCorrUtil;
-  for(int iVz =0 ; iVz < 10 ; ++iVz){
-    for(int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality){
-      delete mPicoEventMixer[iVz][iCentrality];
-    }
-  }
-  //mOutputFileTree->Write();
-  mOutputFileTree->Close();
+StPicoMixedEventMaker::~StPicoMixedEventMaker()
+
+   cout << "Lomnitz:: Destroying" << endl;
+   delete mGRefMultCorrUtil;
+   for (int iVz = 0 ; iVz < 10 ; ++iVz)
+   {
+      for (int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality)
+      {
+         delete mPicoEventMixer[iVz][iCentrality];
+      }
+   }
+   //mOutputFileTree->Write();
+   mOutputFileTree->Close();
 }
 // Method should load Q vector stuff from Hao, needs fixing
 // _________________________________________________________
-bool StPicoMixedEventMaker::loadEventPlaneCorr(Int_t const run) {
-    //needs to implement, will currently break maker
-    return false;
-}
+//bool StPicoMixedEventMaker::loadEventPlaneCorr(StEventPlane const * mEventPlane)
+//{
+//   //needs to implement, will currently break maker
+//   if(!mEventPlane)
+//   {
+//      LOG_WARN << "No EventPlane ! Skipping! " << endm;
+//      return kFALSE;
+//   }
+//   // cout<<"mEventPlane->getAcceptEvent()="<<mEventPlane->getAcceptEvent()<<endl;
+//   if(!mEventPlane->getAcceptEvent()) {
+//      LOG_WARN << "StPicoMixedEvent::THistograms and TProiles NOT found! shoudl check the input Qvector files From HaoQiu ! Skipping this run! " << endm;
+//     return kFALSE;
+//   }
+//   return kTRUE;
+//}
 // _________________________________________________________
-Int_t StPicoMixedEventMaker::Init() {
-    mOutputFileTree->cd();
-    for(int iVz =0 ; iVz < 10 ; ++iVz){
-      for(int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality){
-	mPicoEventMixer[iVz][iCentrality] = new StPicoEventMixer(Form("Cent_%i_Vz_%i",iCentrality,iVz));
-	mPicoEventMixer[iVz][iCentrality]->setEventBuffer(10);
+Int_t StPicoMixedEventMaker::Init()
+{
+   mOutputFileTree->cd();
+   for (int iVz = 0 ; iVz < 10 ; ++iVz)
+   {
+      for (int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality)
+      {
+         mPicoEventMixer[iVz][iCentrality] = new StPicoEventMixer(Form("Cent_%i_Vz_%i", iCentrality, iVz));
+         mPicoEventMixer[iVz][iCentrality]->setEventBuffer(10);
       }
-    }
-    mGRefMultCorrUtil = new StRefMultCorr("grefmult");
-    // if(!LoadEventPlaneCorr(mRunId)){
-    // LOG_WARN << "Event plane calculations unavalable! Skipping"<<endm;
-    // return kStOk;
-    // }
+   }
+   mGRefMultCorrUtil = new StRefMultCorr("grefmult");
+   // if(!LoadEventPlaneCorr(mRunId)){
+   // LOG_WARN << "Event plane calculations unavalable! Skipping"<<endm;
+   // return kStOk;
+   // }
 
-    // -- reset event to be in a defined state
-    //resetEvent();
+   // -- reset event to be in a defined state
+   //resetEvent();
 
-    return kStOK;
+   return kStOK;
 }
 
 // _________________________________________________________
-Int_t StPicoMixedEventMaker::Finish() {
-    // -- Inhertited from StMaker
-    //    NOT TO BE OVERWRITTEN by daughter class
-    //    daughter class should implement FinishHF()
-    mOutputFileTree->cd();
-    for(int iVz =0 ; iVz < 10 ; ++iVz){
-      for(int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality){
-	mPicoEventMixer[iVz][iCentrality]->finish();
-	//delete mPicoEventMixer[iVz][iCentrality];
+Int_t StPicoMixedEventMaker::Finish()
+{
+   // -- Inhertited from StMaker
+   //    NOT TO BE OVERWRITTEN by daughter class
+   //    daughter class should implement FinishHF()
+   mOutputFileTree->cd();
+   for (int iVz = 0 ; iVz < 10 ; ++iVz)
+   {
+      for (int iCentrality = 0 ; iCentrality < 9 ; ++iCentrality)
+      {
+         mPicoEventMixer[iVz][iCentrality]->finish();
+         //delete mPicoEventMixer[iVz][iCentrality];
       }
-    }
-    //mOutputFileTree->Write()
+   }
+   //mOutputFileTree->Write()
 
-    //mOutputFileList->cd();
-    //mOutputFileList->Write(mOutputFileList->GetName(), TObject::kSingleKey);
+   //mOutputFileList->cd();
+   //mOutputFileList->Write(mOutputFileList->GetName(), TObject::kSingleKey);
 
-    //mOutputFileList->Close();
+   //mOutputFileList->Close();
 
-    return kStOK;
+   return kStOK;
 }
 // _________________________________________________________
-void StPicoMixedEventMaker::Clear(Option_t* opt) {
+void StPicoMixedEventMaker::Clear(Option_t* opt)
+{
 }
 // _________________________________________________________
-Int_t StPicoMixedEventMaker::Make() {
+Int_t StPicoMixedEventMaker::Make()
+{
 
-    if(!mPicoDstMaker) {
-        LOG_WARN << "No PicoDstMaker! Skipping! "<<endm;
-        return kStWarn;
-    }
+   if (!mPicoDstMaker)
+   {
+      LOG_WARN << "No PicoDstMaker! Skipping! " << endm;
+      return kStWarn;
+   }
 
-    StPicoDst const* picoDst = mPicoDstMaker->picoDst();
-    if (!picoDst) {
-        LOG_WARN << "No picoDst ! Skipping! "<<endm;
-        return kStWarn;
-    }
-    // - GRef from Guannan
-    if(!mGRefMultCorrUtil) {
-        LOG_WARN << " No mGRefMultCorrUtil! Skip! " << endl;
-        return kStWarn;
-    }
-    //Lomnitz, need to fix this bs
+   StPicoDst const* picoDst = mPicoDstMaker->picoDst();
+   if (!picoDst)
+   {
+      LOG_WARN << "No picoDst ! Skipping! " << endm;
+      return kStWarn;
+   }
+   // - GRef from Guannan
+   if (!mGRefMultCorrUtil)
+   {
+      LOG_WARN << " No mGRefMultCorrUtil! Skip! " << endl;
+      return kStWarn;
+   }
+   //Lomnitz, need to fix this bs
    StThreeVectorF const pVtx = picoDst->event()->primaryVertex();
-    if( fabs(pVtx.z()) >=6.0 )
+   if (fabs(pVtx.z()) >= 6.0)
       return kStOk;
-    mGRefMultCorrUtil->init(picoDst->event()->runId());
-    mGRefMultCorrUtil->initEvent(picoDst->event()->grefMult(),pVtx.z(),picoDst->event()->ZDCx()) ;
-    int const centrality  = mGRefMultCorrUtil->getCentralityBin9();
-    if(centrality < 0 || centrality >8 ) return kStOk;
-    int const vz_bin = (int)((6 +pVtx.z())/1.2) ;
+   mGRefMultCorrUtil->init(picoDst->event()->runId());
+   mGRefMultCorrUtil->initEvent(picoDst->event()->grefMult(), pVtx.z(), picoDst->event()->ZDCx()) ;
+   int const centrality  = mGRefMultCorrUtil->getCentralityBin9();
+   if (centrality < 0 || centrality > 8) return kStOk;
+   int const vz_bin = (int)((6 + pVtx.z()) / 1.2) ;
+   if (vz_bin < 0 || vz_bin > 9) return kStOk;
 //     4            55-60%            30-40%
 //     5            50-55%            20-30%
 //     6            45-50%            10-20%
-    //cout<<"Centrality: "<<centrality<<endl;
-    // - - -
-    if( mPicoEventMixer[vz_bin][centrality] -> addPicoEvent(picoDst) ==  true )
-      mPicoEventMixer[vz_bin][centrality]->mixEvents();
-    //mTree->Fill();
+   //cout<<"Centrality: "<<centrality<<endl;
+//   if(!loadEventPlaneCorr(mEventPlane)){
+//   LOG_WARN << "Event plane calculations unavalable! Skipping"<<endm;
+//   return kStOK;
+//   }
 
-    return kStOk;
+//   cout<<"GUANNAN Xie Check==========="<<endl;
+//   cout<<",yEventPlane->getRunId()="<<mEventPlane->getRunId()<<endl;
+//   cout<<"RunId()="<<picoDst->event()->runId()<<endl;
+//   cout<<"getCentrality()="<<centrality<<endl;
+//   cout<<"mEventPlane->getCentrality()="<<mEventPlane->getCentrality()<<endl;
+//   cout<<"mEventPlane->getEventPlane()="<<mEventPlane->getEventPlane()<<endl;
+
+   if (mPicoEventMixer[vz_bin][centrality] -> addPicoEvent(picoDst) ==  true)
+      mPicoEventMixer[vz_bin][centrality]->mixEvents();
+   //mTree->Fill();
+
+   return kStOk;
 }
 // _________________________________________________________
-Int_t StPicoMixedEventMaker::SetCategories() {
-    return kStOk;
+Int_t StPicoMixedEventMaker::SetCategories()
+{
+   return kStOk;
 }
 // _________________________________________________________
-int StPicoMixedEventMaker::categorize(StPicoDst const * picoDst ) {
-    StThreeVectorF pVertex = (picoDst->event())->primaryVertex();
-    if( fabs(pVertex.z())>6.0 ) return -99;
-    int bin = -6.0 + (pVertex.z()+6.0)/1.2;
-    return bin;
+int StPicoMixedEventMaker::categorize(StPicoDst const * picoDst)
+{
+   StThreeVectorF pVertex = (picoDst->event())->primaryVertex();
+   if (fabs(pVertex.z()) > 6.0) return -99;
+   int bin = -6.0 + (pVertex.z() + 6.0) / 1.2;
+   return bin;
 }
